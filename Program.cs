20aa5b27c33026eb977c5045f685f9c53c45c866
Opilane TTHK <opilane@tthk.ee>
2020-10-12 14:48:39 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Week7Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning!");
+            UserAge();
+           
+        }
+        public static void UserAge()
+        {
+            Console.WriteLine("Enter your year of birth: ");
+            int YearOfBirth = Int32.Parse(Console.ReadLine());
+            int Age = 2020 - YearOfBirth;
+            Console.WriteLine($"You are {Age} years old. ");
+        }
+    }
+}
